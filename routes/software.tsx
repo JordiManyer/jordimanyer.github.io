46d7e5c052897a8711338d8e1cd8029373ec5e93
Jordi Manyer <jordi.manyer@monash.edu>
2026-03-25 18:18:36 +1100

    Added GeoGmsh.jl

--- routes/software.tsx
+++ routes/software.tsx
@@ -15,21 +15,28 @@ export default function Software() {
       description:
         "A next-generation finite element framework in Julia for the grid-based approximation of partial differential equations. The framework provides a high-level API that allows users to write FEM code that closely resembles the mathematical notation.",
       link: "https://github.com/gridap/Gridap.jl",
-      role: "Owner and maintainer",
+      role: "Maintainer and main contributor",
     },
     {
       title: "GridapSolvers.jl",
       description:
         "A collection of solvers and preconditioners specifically designed for the Gridap ecosystem. This package provides efficient numerical methods for solving large-scale linear and nonlinear systems arising from FEM discretizations.",
       link: "https://github.com/gridap/GridapSolvers.jl",
-      role: "Founder, owner and maintainer",
+      role: "Creator, maintainer and main contributor",
     },
     {
       title: "GridapTopOpt.jl",
       description:
         "Level-set-based topology optimisation for the Gridap ecosystem.",
       link: "https://github.com/zjwegert/GridapTopOpt.jl",
-      role: "Founder, owner and maintainer",
+      role: "Creator and main contributor",
+    },
+    {
+      title: "GeoGmsh.jl",
+      description:
+        "Generate Gmsh meshes from geospatial data.",
+      link: "https://github.com/JordiManyer/GeoGmsh.jl",
+      role: "Creator, maintainer and main contributor",
     },
   ];
 
